7cca19f953a73b1a52722b8579a71eed52dc3771
Masoud Safari <mmsafari@gmail.com>
2020-08-20 08:03:13 -0700

    Remove partition in main.py in nn initialization.
    partition is optional parameter. removed for simpler sample.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ClassificationNeuralNetwork import NeuralNetwork
 
-nn = NeuralNetwork([784, 50, 10], 0.1, 'num_class_data.npy', 'num_class_label.npy', [60., 20., 20.])
+nn = NeuralNetwork([784, 50, 10], 0.1, 'num_class_data.npy', 'num_class_label.npy')
 
 nn.test()
 nn.train(1000, 200)
